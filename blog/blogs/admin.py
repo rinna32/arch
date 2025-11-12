871e85d53e763e93dc8482dc2fb57a2311ba2a55
Татьяна Пудова <tapotusha@icloud.com>
2025-11-12 10:46:53 +0300

    удален бэк

--- blog/blogs/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import BlogPost,Entry
-
-# Register your models here.
-admin.site.register(BlogPost)
-admin.site.register(Entry)
